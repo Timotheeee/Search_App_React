f16254deb408c693e972d3820cb51d4c116c45cb
gabrielepace <gabriele.pace@hotmail.com>
2021-05-01 16:12:39 +0200

    Added main navigation

--- Search_App/ContentView.swift
+++ Search_App/ContentView.swift
@@ -8,11 +8,75 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct RepositoryOverview : Decodable, Identifiable {
+    var name: String
+    var description: String
+    var owner: String
+    var avatar_url: String
+    var created_at: String
+    var forks: String
+    var id: String {
+        get {
+            return name;
+        }
     }
+}
+
+struct RepositoryList : Decodable {
+    var repoList: [RepositoryOverview]
+}
+
+struct ContentView: View {
+    
+    @State var repo = [RepositoryList]()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                // TextField("Search for Repositories", text: $repo.name)
+                  //  .textFieldStyle(RoundedBorderTextFieldStyle())
+                  //  .keyboardType(UIKeyboardType.default).padding()
+                
+                NavigationLink(destination: DetailView()) {
+                    Button(action: {
+                        
+                    }) {
+                        Text("Search")
+                    }
+                }
+            }.navigationTitle("GitHub Repo Search")
+            
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    struct DetailView : View {
+        
+        @State var repo = [RepositoryList]()
+        
+        var body: some View {
+            //List ($repo.repoList) { entry in
+                VStack(alignment: .leading) {
+                    //Text("\(entry.name)")
+                    //Text("\(entry.description)").foot(.footnote)
+                }
+        /*}*/.onAppear {
+                // self.repo = loadJSON()!
+            }
+        }
+    }
+}
+
+func loadJSON() -> RepositoryList? {
+    do {
+        if let url = URL(string: "https://api.github.com/search/repositories?q=") {
+            let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            return try decoder.decode(RepositoryList.self, from: data)
+        }
+    } catch {
+        fatalError("Couldn't load file from url bundle:\n\(error)")
+    }
+    return nil
 }
 
 struct ContentView_Previews: PreviewProvider {
